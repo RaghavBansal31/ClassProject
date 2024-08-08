db11bf4376952d84f058bb621940a86a2596fccf
Raghav Bansal <78858559+RaghavBansal31@users.noreply.github.com>
2024-08-08 15:31:57 +0530

    Add files via upload

---
+++ ClassProject/ClassProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassProjectApp.swift
+//  ClassProject
+//
+//  Created by Consultant on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ClassProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
